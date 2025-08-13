1deb9317694b69431b6a20537e3428a498ff8c5b
Abhimanyu <abhimanyuincyber@gmail.com>
2025-08-13 12:30:09 +0000

    change

--- src/main/java/com/mbblab3/controller/MBBLabController.java
+++ src/main/java/com/mbblab3/controller/MBBLabController.java
@@ -19,7 +19,7 @@ public class MBBLabController {
 	@GetMapping("displayAllEmployee")
 	public List<Employee> getAll(){
 		
-		System.out.println("byxzddcv xcvegg");
+		System.out.println("byxzcv xcvegg");
 		
 		List<Employee> list =mService.findAllEmployee();
 			
